7badc6c35c331ee1e4be04a60d1b088a172c60da
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-05-17 18:44:40 +0600

    learn to add data to database

--- index.js
+++ index.js
@@ -25,10 +25,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const coffeeCollection = client.db('coffee_store_DB').collection('coffees');
+
+
+    app.post('/coffees', async(req, res) => {
+        const newCoffee = req.body;
+        
+        const result = await coffeeCollection.insertOne(newCoffee);
+        res.send(result);
+
+    })
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
